bb429dfa952ca925f1b4c811dd36a362d53f441d
Emanuel L.D. Silva <toolsjob.neodeon@gmail.com>
2017-12-26 20:05:56 -0200

    Implements Modal Login

--- admin/index.php
+++ admin/index.php
@@ -2,10 +2,9 @@
 session_start();
 require_once('../_app/config.php');
 $Login = new Login(3); // Nível 3
-echo'<pre>';
-	print_r($Login);
-echo'</pre>';
-
+//echo'<pre>';
+//	print_r($Login);
+//echo'</pre>';
 //Se tiver sessão
 if ($Login->CheckLogin()):
     header('Location: painel.php');
@@ -25,23 +24,25 @@ endif;
 <head>
 	<meta charset="UTF-8">
 	<title>Administração - Video Manager</title>
+    <link rel="stylesheet" href="<?=BASE?>_cdn/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?=BASE?>admin/css/login.css">
 </head>
-<body>
-
-	<form name="AdminLoginForm" action="" method="post">
-                    <label>
-                        <span>E-mail:</span>
-                        <input type="email" name="user" />
-                    </label>
-
-                    <label>
-                        <span>Senha:</span>
-                        <input type="password" name="pass" />
-                    </label>  
-
-                    <input type="submit" name="AdminLogin" value="Logar" class="btn blue" />
-
-                </form>
-	
-</body>
+    <body>
+        <a href="#" class="data-t" data-toggle="modal" data-target="#login-modal">Login</a>
+        <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+            <div class="modal-dialog">
+                <div class="loginmodal-container">
+                    <h1>Administração</h1><br>
+                    <form name="AdminLoginForm" action="" method="post">
+                        <input type="email" name="user" placeholder="Username">
+                        <input type="password" name="pass" placeholder="Password">
+                        <input type="submit" name="AdminLogin" class="login loginmodal-submit" value="Logar">
+                    </form>
+                </div>
+            </div>
+        </div>
+        <script type="text/javascript" src="<?=BASE?>_cdn/jquery.min.js"/></script>
+        <script type="text/javascript" src="<?=BASE?>_cdn/bootstrap/js/bootstrap.min.js"/></script>
+        <script type="text/javascript" src="<?=BASE?>admin/js/login.js"/></script>
+    </body>
 </html>
