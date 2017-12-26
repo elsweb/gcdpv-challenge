5a3eb132fb78a68242cbaeb95c07de6d1276e78c
Emanuel L.D. Silva <toolsjob.neodeon@gmail.com>
2017-12-26 19:41:03 -0200

    Create file - Check class

---
+++ _app/helper/Check.class.php
@@ -0,0 +1,17 @@
+<?php
+class Check {
+
+    private static $Data;
+    private static $Format;
+
+    public static function Email($Email) {
+        self::$Data = (string) $Email;
+        self::$Format = '/[a-z0-9_\.\-]+@[a-z0-9_\.\-]*[a-z0-9_\.\-]+\.[a-z]{2,4}$/';
+
+        if (preg_match(self::$Format, self::$Data)):
+            return true;
+        else:
+            return false;
+        endif;
+    }
+}
