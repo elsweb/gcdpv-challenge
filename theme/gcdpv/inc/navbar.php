a19790ff529c6733e933bd9baa77e220b4e30c8f
Emanuel L.D. Silva <toolsjob.neodeon@gmail.com>
2017-12-27 01:00:35 -0200

    Create theme gcdpv

---
+++ theme/gcdpv/inc/navbar.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Grupo CDPV Videos</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="<?=BASE?>"">Home</a></li>
+    </ul>
+  </div>
+</nav>
